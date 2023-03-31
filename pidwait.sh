8f91dfab4b5b4cbf97ff9ce108f27b313a382dce
Andrés Butelman <abutelman@brinqa.com>
2023-03-31 00:59:21 -0300

    Some fixes

--- pidwait.sh
+++ pidwait.sh
@@ -1,6 +1,8 @@
+#!/usrbin/env bash
+
 function pidwait() {
     PID=$1
-    kill -0 $PID 2>/dev/null
+    kill -0 "$PID" 2>/dev/null
     if [[ $? -eq 0 ]]
     then
         echo "Process $PID still exists, waiting..."  
@@ -9,7 +11,7 @@ function pidwait() {
     while [[ $? -eq 0 ]];
     do
         sleep 1
-        kill -0 $PID 2>/dev/null
+        kill -0 "$PID" 2>/dev/null
     done
     echo "Process $PID has finished."
 }
