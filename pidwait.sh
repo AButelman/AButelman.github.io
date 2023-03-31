a0e216542af2c65a76c7609171e668230bc571c1
Andrés Butelman <abutelman@brinqa.com>
2023-03-31 00:52:30 -0300

    Add shell script with errors to check

---
+++ pidwait.sh
@@ -0,0 +1,15 @@
+function pidwait() {
+    PID=$1
+    kill -0 $PID 2>/dev/null
+    if [[ $? -eq 0 ]]
+    then
+        echo "Process $PID still exists, waiting..."  
+    fi
+    
+    while [[ $? -eq 0 ]];
+    do
+        sleep 1
+        kill -0 $PID 2>/dev/null
+    done
+    echo "Process $PID has finished."
+}
